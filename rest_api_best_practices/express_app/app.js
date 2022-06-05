30e661d5008aa3e91f9f0e88e7e6ac4acad0c544
Imperial-lord <absatyaprakash01@gmail.com>
2022-06-05 05:53:47 +0530

    :sparkles: Add corrected code

--- rest_api_best_practices/express_app/app.js
+++ rest_api_best_practices/express_app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const fs = require('fs');
 
 const app = express();
@@ -10,9 +11,8 @@ app.use(express.json());
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/database.json`));
 
-// Routes
-
-// app.get('/api/v1/users', (req, res) => {
+// Route
+// app.get('/users', (req, res) => {
 //   res.status(200).json({
 //     status: 'success',
 //     results: users.length,
@@ -24,14 +24,15 @@ const users = JSON.parse(fs.readFileSync(`${__dirname}/database.json`));
 
 app.post('/api/v1/users', (req, res) => {
   const newId = users[users.length - 1].id + 1;
-  const newUser = { id: newId, ...req.body };
+  const user = { id: newId, ...req.body };
 
-  users.push(newUser);
+  users.push(user);
+
   fs.writeFile(`${__dirname}/database.json`, JSON.stringify(users), (err) => {
     res.status(201).json({
       status: 'success',
       data: {
-        user: newUser,
+        user,
       },
     });
   });
@@ -39,32 +40,35 @@ app.post('/api/v1/users', (req, res) => {
 
 app.put('/api/v1/users/:id', (req, res) => {
   const { id } = req.params;
-  // code to update a user...
+  console.log(id);
+
   res.status(200).json({
     status: 'success',
     data: {
-      user: '<Updated user info here>',
+      user: '<Updated user info>',
     },
   });
 });
 
 app.delete('/api/v1/users/:id', (req, res) => {
-  const { id } = req.params;
-  // code to delete a user...
   res.status(204).json({
     status: 'success',
-    data: null,
+    data: {
+      user: null,
+    },
   });
 });
 
 app.get('/api/v1/users/:id/name', (req, res) => {
   const { id } = req.params;
 
-  // checks if id for a user exists
+  // check if an user exists for a given id
   const user = users.find((el) => el.id === +id);
+
   if (!user) return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
 
-  const name = users[id].first_name + ' ' + users[id].last_name;
+  const name = users[id - 1].first_name + ' ' + users[id - 1].last_name;
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -75,6 +79,7 @@ app.get('/api/v1/users/:id/name', (req, res) => {
 
 app.get('/api/v1/users', (req, res) => {
   let { first_name, page_number } = req.query;
+
   if (!page_number) page_number = 1;
 
   let results = [...users];
@@ -82,7 +87,6 @@ app.get('/api/v1/users', (req, res) => {
   if (first_name) results = results.filter((el) => el.first_name === first_name);
   const page_size = 25;
   results = results.slice((page_number - 1) * page_size, page_number * page_size);
-
   res.status(200).json({
     status: 'success',
     results: results.length,
@@ -93,4 +97,4 @@ app.get('/api/v1/users', (req, res) => {
 });
 
 // Server
-app.listen(3000, () => console.log('Server running'));
+app.listen(3000, () => console.log('Server is running!'));
